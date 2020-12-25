1078c66db8779958307452f0c2d02be147713133
sarana200 <47283850+sarana200@users.noreply.github.com>
2020-12-25 21:34:45 +0300

    اشتغلت

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ maze3 = [[0 for x in range(20)] for y in range(20)]
 maze4 = [[0 for x in range(20)] for y in range(20)]
 pacmanRotate = 0
 
+
+def key_pressed(key, x, y):
+    if key == b'\033':
+        sys.exit()
+
+
 class Main:
     def draw(self):
         global maze1, maze2, maze3, maze4
@@ -31,14 +37,14 @@ class Main:
         Read_text = readfromoutside.Readfromoutside()
         Draw_maze = map.Map()
         Call_coin = coin.Coin()
-        Call_pacman = pacman.PacMan()
+        Call_pacman = pacman.PacMan(7.6, 0.4, 1)
 
-        #gluLookAt(0.0, 0.0,8.0, -2.0, 1.0, 0.0, 2.0, 50.0, -8.0)
-        gluLookAt(0.0, -1.0,8.0, -2.0, 0.0, 4.0, -9.0, 156.0, 17.0)
+        # gluLookAt(0.0, 0.0,8.0, -2.0, 1.0, 0.0, 2.0, 50.0, -8.0)
+        gluLookAt(0.0, -1.0, 8.0, -2.0, 0.0, 4.0, -9.0, 156.0, 17.0)
         if Call_coin.score() == 0:
             # draw pacman
             glPushMatrix()
-            Call_pacman.drawPacman(7.6, 0.4, 1)
+            Call_pacman.drawPacman()
             glPopMatrix()
             glPushMatrix()
             maze1 = Read_text.readObj("maze1.txt", 1)
@@ -48,7 +54,7 @@ class Main:
         if Call_coin.score() > 500:
             # draw pacman
             glPushMatrix()
-            Call_pacman.drawPacman(7.6, 0.4, 1)
+            Call_pacman.drawPacman()
             glPopMatrix()
             glPushMatrix()
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -59,7 +65,7 @@ class Main:
         if Call_coin.score() > 100:
             # draw pacman
             glPushMatrix()
-            Call_pacman.drawPacman(7.6, 0.4, 1)
+            Call_pacman.drawPacman()
             glPopMatrix()
             glPushMatrix()
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -70,7 +76,7 @@ class Main:
         if Call_coin.score() > 150:
             # draw pacman
             glPushMatrix()
-            Call_pacman.drawPacman(7.6, 0.4, 1)
+            Call_pacman.drawPacman()
             glPopMatrix()
             glPushMatrix()
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -79,8 +85,6 @@ class Main:
             Call_coin.drawcoin(0, 0, maze4)
             glPopMatrix()
         glFlush()
-
-
 
     def InitGL(self):
         glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -96,7 +100,7 @@ class Main:
 
 def main():
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH )
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH)
     glutInitWindowSize(1000, 650)
     glutInitWindowPosition(0, 0)
     glutCreateWindow(b"PACMAN GAME NEW UPDATE")
@@ -107,10 +111,12 @@ def main():
     # glutMouseWheelFunc(MouseWheel)
     # glutMotionFunc(MouseWheelMotion)
     # Register the function called when the keyboard is pressed.
-    glutKeyboardFunc(pacman.PacMan.key_pressed)
-    glutSpecialFunc(pacman.PacMan.key_pressed_special)
-    glutSpecialUpFunc(pacman.PacMan.key_pressed_special_up)
+    Call_pacman = pacman.PacMan(7.6, 0.4, 1)
+    glutKeyboardFunc(Call_pacman.key_pressed)
+    glutSpecialFunc(Call_pacman.key_pressed_special)
+    glutSpecialUpFunc(Call_pacman.key_pressed_special_up)
     start_game.InitGL()
     glutMainLoop()
+
 
 main()
